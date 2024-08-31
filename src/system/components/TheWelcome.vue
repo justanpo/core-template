980d8484d49eaa242c72b1fbfcd43c7eb1bc0fa8
a.poznyakov <me@anpostudio.ru>
2024-08-31 23:57:56 +0200

    init

---
+++ src/system/components/TheWelcome.vue
@@ -0,0 +1,23 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      1
+    </template>
+    <template #heading>One</template>
+
+    text
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      2
+    </template>
+    <template #heading>Two</template>
+
+    text text
+  </WelcomeItem>
+</template>
