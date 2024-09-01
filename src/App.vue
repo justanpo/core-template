1fecbcb1bb0a66157a448e28b960d66908336ef9
a.poznyakov <me@anpostudio.ru>
2024-09-01 02:50:17 +0200

    change root alias

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HelloWorld from '@/system/components/HelloWorld.vue'
-import TheWelcome from '@/system/components/TheWelcome.vue'
+import HelloWorld from '@root/system/components/HelloWorld.vue'
+import TheWelcome from '@root/system/components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="system/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@root/system/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="Sysplate" />
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '@/system/assets/main.css'
+import '@root/system/assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
